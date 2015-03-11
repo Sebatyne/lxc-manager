d89b8216319df51469ea9967e619c44ae7e6056c
Nicolas Wavrant <nicolas.wavrant@gmail.com>
2015-03-11 20:16:38 +0100

    lxcreate: create basic lxc config file

--- lxcreate.sh
+++ lxcreate.sh
@@ -1,1 +1,78 @@
 #!/bin/bash
+
+BRIDGE_NAME="lxbr0"
+LXPATH="$HOME/.lxcontainers"
+
+######################################################################
+# Argument Parser
+######################################################################
+while getopts "i:g:b:n:h" opt; do
+    case $opt in
+        i)
+            IP=$OPTARG
+            ;;
+        g)
+            GATEWAY_IP=$OPTARG
+            ;;
+        n)
+            VM_NAME=$OPTARG
+            ;;
+        h)
+            print_help
+            ;;
+        \?)
+            echo "Invalid option . -$OPTARG"
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+######################################################################
+# Function declarations
+######################################################################
+print_help() {
+    cat > /dev/stdout << EOF
+lxcreate -i IP_CIDR -g GATEWAY_IP -n NAME
+EOF
+}
+
+######################################################################
+# Common checks
+######################################################################
+# Abort if not all mandatory parameters are not set
+if [ -z $IP ] || [ -z $GATEWAY_IP ] || [ -z $VM_NAME ];
+then
+    echo "Please give all mandatory arguments"
+    print_help
+    exit 1
+fi
+
+if [ -d $LXPATH/$VM_NAME ];
+then
+    echo "$LXPATH/$VM_NAME already exists, you cannot create a VM"
+else
+    mkdir -p $LXPATH/$VM_NAME/rootfs
+fi
+
+######################################################################
+# Main
+######################################################################
+# Write config file
+cat > $LXPATH/$VM_NAME/config << EOF
+lxc.network.type = veth
+lxc.network.flags = up
+lxc.network.link = $BRIDGE_NAME
+lxc.rootfs = $LXPATH/$VM_NAME/rootfs
+# IP is a CIDR
+lxc.network.ipv4 = $IP
+lxc.network.ipv4.gateway = $GATEWAY_IP
+
+# Common configuration
+lxc.include = /usr/share/lxc/config/debian.common.conf
+
+# Container specific configuration
+lxc.mount = $LXPATH/$VM_NAME/fstab
+lxc.utsname = $VM_NAME
+lxc.arch = amd64
+EOF
